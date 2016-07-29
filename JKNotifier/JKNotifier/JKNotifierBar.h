40febc94cc13c3ac546845ea931674f3dcc6826c
zhangqinghe <xd01051235@163.com>
2016-07-29 17:13:32 +0800

    修改iOS7/iOS8横屏时候不能弹出的bug／并修正正在显示notifierBar时旋转屏幕的逻辑处理

--- JKNotifier/JKNotifier/JKNotifierBar.h
+++ JKNotifier/JKNotifier/JKNotifierBar.h
@@ -9,11 +9,18 @@
 #import <UIKit/UIKit.h>
 @class JKNotifier;
 @class JKNotifierBar;
+
+#define  LESS_IOS0_AND_ORIENTATION_PORTRAIT   [[UIDevice currentDevice].systemVersion  floatValue] <9.0 && \
+[UIApplication sharedApplication].statusBarOrientation != UIInterfaceOrientationPortrait
+
+
 typedef void(^JKNotifierBarClickBlock)(NSString *name,NSString *detail,JKNotifier *notifier);
 @interface JKNotifierBar : UIWindow
 {
     JKNotifierBarClickBlock _notifierBarClickBlock;
 }
+@property(nonatomic,assign)BOOL isDissiming;
+@property(nonatomic,assign)UIInterfaceOrientation orientation;
 - (void)show:(NSString*)note name:(NSString*)appName icon:(UIImage*)appIcon;
 
 - (void)handleClickAction:(JKNotifierBarClickBlock)notifierBarClickBlock;
